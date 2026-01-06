ea45fdc4f367671b1ce4d6a090e40b35f5aeb911
ragahavsihag-404 <raghav.25076@ce.du.ac.in>
2026-01-07 00:55:17 +0530

    4_loops

---
+++ Basics/four_loops.java
@@ -0,0 +1,37 @@
+public class four_loops {
+    public static void main(String[] args){
+//for loop
+/*for(initialization; condition; updation){
+
+do something
+
+}*/
+
+for(int i=0; i<100; i=i+1){
+    //i=i+1 = i++
+    System.out.println("Hello");
+}
+
+//while loop
+/*while(condition){
+do something
+updation
+} */
+int n=0;
+while(n<100){
+    System.out.println("Hello");
+    n++;
+}
+
+//do-while loop
+/*do{
+do something
+} 
+while(condition)
+*/
+
+    }
+    
+}
+
+
